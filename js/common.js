276f28f6b3fbba33266160306a587d6ba15101c8
liang <44829192+LGHXG@users.noreply.github.com>
2019-03-26 13:34:27 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,26 +1,68 @@
 $(document).ready(function () {
 
     //banner
-    $('#banner').easyFader();
+    (function banner() {
+        var index = 0;
+        var flag = true;
+        // 下一张
+        function next() {
+            if (flag) {
+                flag = false;
+                index--;
+                $('.banner-img li').each(function (key, value) {
+                    $(this).css({
+                        "transform":"rotateX("+ (index * 90) +"deg)",
+                        "transition-delay":(key * 0.1) + "s"
+                    })
+                });
+                var timeId = setTimeout(function () {
+                    flag = true
+                }, 500)
+            }
+        }
+        setInterval(next,1800);
+        $('.banner .next').on('click', next);
+        // 上一张
+        $('.banner .pre').on('click', function () {
+            if (flag) {
+                flag = false;
+                index--;
+                $('.banner-img li').each(function (key, value) {
+                    $(this).css({
+                        "transform":"rotateX("+ (-index * 90) +"deg)",
+                        "transition-delay":(key * 0.2) + "s"
+                    })
+                })
+                var timeId = setTimeout(function () {
+                    flag = true
+                }, 1000)
+            }
+        })
+    }());
 
     // nav 隐藏
-    var header = $("header");
-    $(window).scroll(function(){
-        if( $(document).scrollTop() >= 124 ){
-            header.addClass("slideUp");
-        } else {
-            header.removeClass("slideUp");
-            header.addClass("slideDown");
-        }
-    });
-    
-    
-    var logo = $('.logo');
-    $('.branding').mouseenter(function () {
-        logo.animate({left: 6},500);
-        setTimeout(function () {
-            logo.animate({left: 0},500);
-        },500)
-    });
+    (function navshow() {
+        var header = $("header");
+        $(window).scroll(function(){
+            if( $(document).scrollTop() >= 124 ){
+                header.addClass("slideUp");
+            } else {
+                header.removeClass("slideUp");
+                header.addClass("slideDown");
+            }
+        });
+
+
+        var logo = $('.logo');
+        $('.branding').mouseenter(function () {
+            logo.animate({left: 6},500);
+            setTimeout(function () {
+                logo.animate({left: 0},500);
+            },500)
+        });
+    }())
+
+    // blogimg提示
+
 
 });
